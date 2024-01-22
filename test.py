751268e5028cb526dd0029aee9c8066bcbdd42e1
cubedice <cubedice725@gmail.com>
2024-01-22 18:14:28 +0900

    완료

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 def add(a,b):
-
+    return print(a + b)
+    
 def sub(a,b):
 
 def main():
-    
+    a = float(input("a의 값을 입력하세요 : "))
+    b = float(input("b의 값을 입력하세요 : "))
+    add(x,y)
+
+
 if __name__ == "__main__":
     main()
